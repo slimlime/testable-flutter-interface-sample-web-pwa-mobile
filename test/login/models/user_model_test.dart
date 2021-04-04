b20739019974bbfb947466d9438936c5e242defd
ml <30815731+slimlime@users.noreply.github.com>
2021-04-04 10:44:28 +1000

    test(test,login/models): add user model test names

---
+++ test/login/models/user_model_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('user', () {
+    test('can construct', () {
+      
+    });
+
+    test('can construct from json', () {
+
+    });
+
+    test('can encode to json', () {
+      
+
+    });
+  });
+}
