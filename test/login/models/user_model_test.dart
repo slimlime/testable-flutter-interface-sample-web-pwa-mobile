159df8decd68467101e272b4ff1a791c7d308d99
ml <30815731+slimlime@users.noreply.github.com>
2021-04-04 11:20:33 +1000

    test(user_model_test): add expected test equatable

--- test/login/models/user_model_test.dart
+++ test/login/models/user_model_test.dart
@@ -8,6 +8,9 @@ void main() {
   final Faker faker = Faker();
 
   group('user', () {
+    final String sameName = faker.person.name();
+    final String sameEmail = faker.internet.email();
+
     test('can construct', () {
       final User user = User(
         faker.person.name(),
@@ -16,6 +19,17 @@ void main() {
     });
 
     test('can construct from json', () {
+      final User user = User(
+        faker.person.name(),
+        faker.internet.email(),
+      );
+
+      final Map<String, dynamic> jsonA = user.toJson();
+
+      final User parsedUserFromJson = User.fromJson(jsonA);
+    });
+
+    test('unable to construct from invalid json', () {
       final User user = User(
         faker.person.name(),
         faker.internet.email(),
@@ -38,8 +52,52 @@ void main() {
       final String jsonC = json.encode(user);
     });
 
-    test('is value equal to', () {});
+    test('same values equal', () {
+      final String sameName = faker.person.name();
+      final String sameEmail = faker.internet.email();
 
-    test('is hash equal to', () {});
+      final User userA = User(
+        sameName,
+        sameEmail,
+      );
+      final User userB = User(
+        sameName,
+        sameEmail,
+      );
+
+      final bool isSameUser = userA == userB;
+
+      expect(isSameUser, true);
+
+      // Check optional consistency null potential strangeness with hash compute.
+    });
+
+    test('different values inequal', () {
+      final String sameName = faker.person.name();
+      final String sameEmail = faker.internet.email();
+    });
+
+    test('same values hash equal', () {
+      final String sameName = faker.person.name();
+      final String sameEmail = faker.internet.email();
+
+      final User userA = User(
+        sameName,
+        sameEmail,
+      );
+      final User userB = User(
+        sameName,
+        sameEmail,
+      );
+
+      final bool isSameUser = userA == userB;
+
+      expect(isSameUser, true);
+    });
+
+    test('different values hash inequal', () {
+      final String sameName = faker.person.name();
+      final String sameEmail = faker.internet.email();
+    });
   });
 }
