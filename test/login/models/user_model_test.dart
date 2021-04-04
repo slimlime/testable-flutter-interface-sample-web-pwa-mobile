ce6572f36fba18094876d4eddffdbbbf618bb9a8
ml <30815731+slimlime@users.noreply.github.com>
2021-04-04 11:03:29 +1000

    test(user_model_test): add user constructor jsonable base tests

--- test/login/models/user_model_test.dart
+++ test/login/models/user_model_test.dart
@@ -1,18 +1,41 @@
+import 'dart:convert';
+
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:testable_web_app/login/models/user_model.dart';
 
 void main() {
+  const Faker faker = Faker();
+
   group('user', () {
     test('can construct', () {
-      
+      final User user = User(
+        faker.person.name(),
+        faker.internet.email(),
+      );
     });
 
     test('can construct from json', () {
+      final User user = User(
+        faker.person.name(),
+        faker.internet.email(),
+      );
 
+      final Map<String, dynamic> jsonA = user.toJson();
+
+      final User parsedUserFromJson = User.fromJson(jsonA);
     });
 
     test('can encode to json', () {
-      
+      final User user = User(
+        faker.person.name(),
+        faker.internet.email(),
+      );
+      user.toJson();
+      jsonEncode(user);
 
+      final String jsonB = jsonEncode(user);
+      final String jsonC = json.encode(user);
     });
   });
 }
